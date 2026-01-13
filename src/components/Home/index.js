b90a2203b495139a63b3cf567aa57b5d73066bee
vali-syed <chinnavalisyed@gmail.com>
2026-01-13 23:11:49 +0530

    Project setup and home page developed

---
+++ src/components/Home/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+const Home = () => (
+  <div>
+    <div className="home-container">
+        <div className="home-page">
+            <h1 className="main-heading">find the job that fits your life</h1>
+            <p className="home-description"> 
+             Millions of people are searching for jobs, salary information, company
+             reviews. Find the job that fits your abilities and potential.
+          </p>
+          <button className="find-jobs-button"   >Find Job</button>
+        </div>
+    </div>
+  </div>
+)
+
+export default Home;
