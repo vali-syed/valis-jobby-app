888fb06fa45d630450e47750e4e745699e3413bf
vali-syed <chinnavalisyed@gmail.com>
2026-01-16 22:37:31 +0530

    navigation is added in header to home and other pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,19 @@
+import {Link} from 'react-router-dom'
+
 import './index.css'
 
 const Header = () => (
     <nav className="navbar">
         <div className="nav-container">
-            <img src="https://assets.ccbp.in/frontend/react-js/logo-img.png" alt="logo" className='website-logo'/>
+            <Link to="/">
+            <img 
+                src="https://assets.ccbp.in/frontend/react-js/logo-img.png" 
+                alt="logo" 
+                className='website-logo'
+            />
+            </Link>
             <ul className='nav-items'>
-                <li className="nav-item">Home</li>
+                <li className="nav-item"><Link to="/">Home</Link></li>
                 <li className="nav-item">Jobs</li>
             </ul>
             <button className="logout-button">Logout</button>
